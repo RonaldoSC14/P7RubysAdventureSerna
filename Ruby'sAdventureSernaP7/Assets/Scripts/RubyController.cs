0bb12bce1d90a00084be596dee345430993eb0f0
Serna <759046@student.dusd.net>
2023-11-22 23:13:43 -0800

    Projectile
    Created the projectile, the physics system, and launched the projectile script.

--- Ruby'sAdventureSernaP7/Assets/Scripts/RubyController.cs
+++ Ruby'sAdventureSernaP7/Assets/Scripts/RubyController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
@@ -9,11 +10,13 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
+
+    public GameObject projectilePrefab;
 
     public int health { get { return currentHealth; }}
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
 
@@ -53,9 +56,14 @@ public class RubyController : MonoBehaviour
 
         if (isInvincible )
         {
-            invincibleTimer = Time.deltaTime;
+            invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0 )
                 isInvincible = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
         }
     }
     void FixedUpdate()
@@ -70,15 +78,25 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
-            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
         }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-   
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+
+    }
 }
